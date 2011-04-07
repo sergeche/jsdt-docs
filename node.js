5b1f8ee662352776adcd2d99335425520351b1d1
Sergey Chikuyonok <serge.che@gmail.com>
2011-04-07 15:25:40 +0400

    Added path module to Node.js

--- node.js
+++ node.js
@@ -2430,3 +2430,190 @@ fs.Stats.prototype = {
 	/** @returns Boolean */
 	isSocket: function(){return true;}
 };
+/**
+ * This module contains utilities for dealing with file paths. Use
+ * require('path') to use it.
+ * 
+ * @constructor
+ */
+function NodePathModule() {};
+
+/**
+ * Normalize a string path, taking care of '..' and '.' parts. When multiple
+ * slashes are found, they're replaces by a single one; when the path contains a
+ * trailing slash, it is preserved. On windows backslashes are used.
+ * <p>
+ * Example:
+ * </p>
+ * 
+ * <pre>
+ * path.normalize('/foo/bar//baz/asdf/quux/..')
+ * // returns '/foo/bar/baz/asdf'
+ * </pre>
+ * 
+ * @param {String}
+ *            p
+ * @returns {String}
+ */
+NodePathModule.prototype.normalize = function(p){return "";};
+
+/**
+ * <p>
+ * Join all arguments together and normalize the resulting path.
+ * </p>
+ * <p>
+ * Example:
+ * </p>
+ * 
+ * <pre>
+ * require('path').join('/foo', 'bar', 'baz/asdf', 'quux', '..')
+ * '/foo/bar/baz/asdf'
+ * </pre>
+ * @param {String} paths
+ * @returns {String}
+ */
+NodePathModule.prototype.join = function(paths){return "";};
+
+/**
+ * <p>
+ * Resolves to an absolute path.
+ * </p>
+ * <p>
+ * If to isn't already absolute from arguments are prepended in right to left
+ * order, until an absolute path is found. If after using all from paths still
+ * no absolute path is found, the current working directory is used as well. The
+ * resulting path is normalized, and trailing slashes are removed unless the
+ * path gets resolved to the root directory.
+ * </p>
+ * <p>
+ * Another way to think of it is as a sequence of cd commands in a shell.
+ * </p>
+ * 
+ * <pre>
+ * path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile')
+ * </pre>
+ * 
+ * <p>
+ * Is similar to:
+ * </p>
+ * 
+ * <pre>
+ * cd foo/bar
+ * cd /tmp/file/
+ * cd ..
+ * cd a/../subfile
+ * pwd
+ * </pre>
+ * 
+ * <p>
+ * The difference is that the different paths don't need to exist and may also
+ * be files.
+ * </p>
+ * <p>
+ * Examples:
+ * </p>
+ * 
+ * <pre>
+ * path.resolve('/foo/bar', './baz')
+ * // returns
+ * '/foo/bar/baz'
+ * 
+ * path.resolve('/foo/bar', '/tmp/file/')
+ * // returns
+ * '/tmp/file'
+ * 
+ * path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif')
+ * // if currently in /home/myself/node, it returns
+ * '/home/myself/node/wwwroot/static_files/gif/image.gif'
+ * </pre>
+ * 
+ * @param {String}
+ *            from
+ * @param {String}
+ *            to
+ * @returns {String}
+ */
+NodePathModule.prototype.resolve = function(from, to){return "";};
+
+/**
+ * <p>
+ * Return the directory name of a path. Similar to the Unix dirname command.
+ * </p>
+ * <p>
+ * Example:
+ * </p>
+ * 
+ * <pre>
+ * path.dirname('/foo/bar/baz/asdf/quux')
+ * // returns '/foo/bar/baz/asdf'
+ * </pre>
+ * 
+ * @param {String} p
+ * @returns {String}
+ */
+NodePathModule.prototype.dirname = function(p){return "";};
+
+/**
+ * <p>
+ * Return the last portion of a <code>path</code>. Similar to the Unix
+ * <code>basename</code> command.
+ * </p>
+ * <p>
+ * Example:
+ * </p>
+ * 
+ * <pre>
+ * path.basename('/foo/bar/baz/asdf/quux.html')
+ * // returns
+ * 'quux.html'
+ * 
+ * path.basename('/foo/bar/baz/asdf/quux.html', '.html')
+ * // returns
+ * 'quux'
+ * </pre>
+ * @param {String} path
+ * @param {String} ext
+ * @returns {String}
+ */
+NodePathModule.prototype.basename = function(path, ext){return "";};
+
+/**
+ * Return the extension of the path. Everything after the last '.' in the last
+ * portion of the path. If there is no '.' in the last portion of the path or
+ * the only '.' is the first character, then it returns an empty string.
+ * Examples:
+ * 
+ * <pre>
+ * path.extname('index.html')
+ * // returns
+ * '.html'
+ * 
+ * path.extname('index')
+ * // returns
+ * ''
+ * </pre>
+ * @param {String} p
+ */
+NodePathModule.prototype.extname = function(p){return "";};
+
+/**
+ * Test whether or not the given path exists. Then, call the callback argument
+ * with either true or false. Example:
+ * 
+ * <pre>
+ * path.exists('/etc/passwd', function(exists) {
+ * 	util.debug(exists ? &quot;it's there&quot; : &quot;no passwd!&quot;);
+ * });
+ * </pre>
+ * 
+ */
+
+NodePathModule.prototype.exists = function(p, callback){};
+/**
+ * Synchronous version of {@link NodePathModule#exists}
+ * @param {String} p
+ * @returns Boolean
+ */
+NodePathModule.prototype.existsSync = function(p){return true;};
+
+var path = new NodePathModule;
